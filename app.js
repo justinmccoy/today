2eb61a6460982fbaedc4276ac3efbe2600b36492
Justin McCoy <earth@justins-air.lan>
2022-07-16 01:13:49 -0500

    weball app pulls from url, hardcoded to bucket in cos

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
 const express = require("express");
+const ejs = require("ejs");
+const path = require("path");
+const fs = require("fs");
+
 const app = express();
+const dirPath = path.join(__dirname, "public/pdfs");
 const port = 80;
 
-app.get("/", (req,res) => res.send("Hello World!"));
+/*const files = fs.readdirSync(dirPath).map(name => {
+	return {
+		name: path.basename(name, ".pdf"),
+		url: `/pdfs/${name}`
+		};
+});
+*/
+
+const files = {
+		name: "pdf",
+		url: `https://pdfs-knfa2.s3.us-east.cloud-object-storage.appdomain.cloud/wallstreetjournal_20220715_TheWallStreetJournal.pdf`
+};
+
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 
-app.listen(port, () => console.log('app listening on port ${port}'));
+app.get("/",(req, res) => {
+	res.render("index", files);
+});
 
+
+app.listen(port, () => console.log(`app listening on port ${port}`));
